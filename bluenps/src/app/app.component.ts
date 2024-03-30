39f007542176e191dbf6975445b6f67949950f09
DoodleyJC <dijkstradesley@gmail.com>
2024-03-31 00:09:19 +0100

    test

--- bluenps/src/app/app.component.ts
+++ bluenps/src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'bluenps';
+
+
+  submitCourtName(): void{
+    var e = (document.getElementById("courtSelection") as HTMLInputElement);
+    var court = e.value;
+    var name = (document.getElementById("nameInput") as HTMLInputElement).value;
+    var date = new Date();
+    alert(date.getTime());
+  }
+  
+
+
+
 }
+
+
+
+
+
+
